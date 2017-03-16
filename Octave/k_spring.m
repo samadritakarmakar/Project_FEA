03629ea958c1af301ecc9d44edc31f6a70e869c1
Samadrita <samadritakarmakar@gmail.com>
2017-03-16 22:21:58 +0530

    Commented and length fucntion change in x_solve.m

--- Octave/k_spring.m
+++ Octave/k_spring.m
@@ -1,11 +1,16 @@
+#Used to put values of k in the global Stiffness matrix K.
+#Takes spring stiffness, angle with x axis, node number 1, node number 2 and 
+#total number of nodes as argument.
 function k=k_spring(k_stiff,theta,node1,node2,node_t)
-C=cosd(theta);
+C=cosd(theta); 
 S=sind(theta);
 k=zeros(2*node_t);
+#The right positions of k generally ranges from 2n-1 to 2n where n is node number.
 n1=2*node1;
 n2=2*node2;
 lambda=k_stiff*[C^2,C*S;C*S,S^2];
-k(n1-1:n1,n1-1:n1)=lambda;
+#Puts the values in the right positions.
+k(n1-1:n1,n1-1:n1)=lambda; 
 k(n1-1:n1,n2-1:n2)=-lambda;
 k(n2-1:n2,n1-1:n1)=-lambda;
 k(n2-1:n2,n2-1:n2)=lambda;
--- Octave/k_truss.m
+++ Octave/k_truss.m
@@ -1,10 +1,15 @@
+#Used to put values of stiffness of Truss in the global Stiffness matrix K.
+#Takes Area, Elasticity, Length, angle with x axis, node number 1, node number 2 and 
+#total number of nodes as argument.
 function k=k_truss(A,E,L,theta,node1,node2,node_t)
 C=cosd(theta);
 S=sind(theta);
 k=zeros(2*node_t);
+#The right positions of k generally ranges from 2n-1 to 2n where n is node number.
 n1=2*node1;
 n2=2*node2;
 lambda=A*E/L*[C^2,C*S;C*S,S^2];
+#Puts the values in the right positions.
 k(n1-1:n1,n1-1:n1)=lambda;
 k(n1-1:n1,n2-1:n2)=-lambda;
 k(n2-1:n2,n1-1:n1)=-lambda;
